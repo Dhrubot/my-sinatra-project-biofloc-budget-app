4ae18be9c8111fbb56506dde2f1ece6d659a68fd
Dhrubot <dhrubo.dh@gmail.com>
2020-06-03 16:08:30 -0400

    swrote has many through assosciations for user and campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,5 @@
 class Campaign < ActiveRecord::Base
     belongs_to :organizer, :class_name => "User"
     has_many :donations
+    has_many :donors, through: :donations, source: :donor
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    
     has_many :organized_campaigns, :class_name => "Campaign", foreign_key: "organizer_id"
-    has_many :donations
+    has_many :donations, foreign_key: "donor_id"
+    has_many :donated_campaigns, through: :donations, source: :campaign
 end
