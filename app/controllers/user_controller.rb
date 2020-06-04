879a2b1d00834b7c624763e26ff425f6ab4588b2
Dhrubot <dhrubo.dh@gmail.com>
2020-06-04 06:42:31 -0400

    created user controller signup route and enabled sessions

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,8 +3,10 @@ class UserController < ApplicationController
 
     get '/signup' do
         #should show options for signup
-        
+        erb :'users/new'
     end
 
-
+    post '/users' do
+        raise params.inspect
+    end
 end
