c58aab7914b786c6ae4786f242aeb3d47934bbe8
Dhrubot <dhrubo.dh@gmail.com>
2020-06-04 07:00:51 -0400

    Added signup and login routes and view page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,6 +7,21 @@ class UserController < ApplicationController
     end
 
     post '/users' do
-        raise params.inspect
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        if @user.save
+            redirect to "/users/#{@user.id}" #will need to refractor this to redirect to the username.
+        else
+            redirect to '/signup'
+        end
+    end
+
+    get '/login' do
+        erb :'/users/login'
+    end
+
+    get '/users/:id' do
+        @user = User.find(params[:id])
+
+        erb :'/users/index'
     end
 end
